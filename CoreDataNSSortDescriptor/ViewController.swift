ba42bba1edf439d4dcad7a987781041d31b6004d
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-04-03 19:56:32 +0200

    Initial Commit

---
+++ CoreDataNSSortDescriptor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataNSSortDescriptor
+//
+//  Created by Kresimir Ivanjko on 03.04.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
